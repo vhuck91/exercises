a0761fba11f29235c3083ea5e6b01fe10da331f5
Vadim Huck <back.end@hotmail.com>
2020-05-13 10:02:23 +0200

    vhuck first commit

---
+++ PB-language-statements/homework.js
@@ -0,0 +1,22 @@
+
+console.log('hello worlds 1');
+// [ctrl] + [shift] + [7] console.log('This is my first js file');
+
+console.log('Hello Worlds 2' /* + ' This is my first JS file!'*/)
+/*
+1.syntax error appear cuz we dont have a proper syntax declaration code becomes invalid
+2.Error is present (syntax) after fixing thatcd
+*/
+
+
+/* to much, even tho its just removing content of ()
+var str = "Hello worlds (This is my first js file)";
+str = str.replace(/ \([\s\S]*?\)/g, '');
+console.log(str);
+
+SOLUTION 2 - aka string plus 1
+var Str = "Hello Worlds."
+Str += "this is my First JS file!"
+
+console.log(Str);
+*/
