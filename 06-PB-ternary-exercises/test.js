49efca40a176c788ba2f8456f68a960f3aed277f
Vadim Huck <back.end@hotmail.com>
2020-05-18 15:52:08 +0200

    adding my code

--- 06-PB-ternary-exercises/test.js
+++ 06-PB-ternary-exercises/test.js
@@ -1,7 +1,7 @@
 // 1
-let isDog;
+let isDog = true;
 
-console.log("1 Anwser //")
+console.log("1 Anwser //");
 console.log(isDog ? "pat pat" : "find me a dog to pat!")
 
 /*
@@ -9,24 +9,23 @@ let dog = true;
 (name === true) ? console.log("pat pat") : console.log("find me a dog to pat!");
 */
 
-
-
 // 2
-let speedCheck
+let speedCheck = 40;
+let speedLimit =50;
 
-console.log("2 Anwser //")
+console.log("2 Anwser //");
 console.log(speedCheck >= 50 ? "You are going to fast" : "Oma!!!");
 
 // 3
-let age
+let age = 21;
 
 console.log("3 Anwser //")
 console.log(age < 16 ? "Butter Beer" : "Beer");
 
 // 4
-let isStudent;
+let isStudent = false;
 console.log("4 Anwser //")
-console.log(isStudent ? "4. Ticket costs €5,00" : "4.", "Ticket costs €12,00");
+console.log(isStudent ? "Ticket costs €5,00" : "Ticket costs €12,00");
 
 /*
 let isStudent;
@@ -34,11 +33,10 @@ let isStudent;
         console.log("Tiket cost €5,00");
     }
 else{
-        console.log("Ticket costs €12,00"); // confused cuz we dont have var = y exept y
-    }
+        console.log("Ticket costs €12,00");
 */
 
 // 5
-let okMarie;
+let okMarie = true; // wait does it mean if marie has a cake?
 console.log("5 Anwser //")
-console.log(okMarie === "cake" ? "Let them eat cake" : "Oh, bother");
+console.log(okMarie ? "Let them eat cake" : "Oh, bother");
