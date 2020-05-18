d2feff0f83073feb786b0d56eea2d8bf4629651b
Vadim Huck <back.end@hotmail.com>
2020-05-18 11:13:53 +0200

    insert extra task

--- 06-PB-ternary-exercises/test.js
+++ 06-PB-ternary-exercises/test.js
@@ -4,6 +4,14 @@ let isDog;
 console.log("1 Anwser //")
 console.log(isDog ? "pat pat" : "find me a dog to pat!")
 
+/*
+let dog = true;
+
+(name === true) ? console.log("pat pat") : console.log("find me a dog to pat!");
+*/
+
+
+
 // 2
 let speedCheck
 
@@ -18,6 +26,7 @@ console.log(age < 16 ? "Butter Beer" : "Beer");
 
 // 4
 let isStudent;
+console.log("4 Anwser //")
 console.log(isStudent ? "4. Ticket costs €5,00" : "4.", "Ticket costs €12,00");
 
 /*
@@ -32,5 +41,5 @@ else{
 
 // 5
 let okMarie;
-
+console.log("5 Anwser //")
 console.log(okMarie === "cake" ? "Let them eat cake" : "Oh, bother");
