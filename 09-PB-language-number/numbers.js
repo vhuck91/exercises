33ad77b4c6c37e25091fa0ec6ba27de3879cf7ac
Vadim Huck <back.end@hotmail.com>
2020-05-20 10:53:35 +0200

    code after feed back of teacher

--- 09-PB-language-number/numbers.js
+++ 09-PB-language-number/numbers.js
@@ -1,9 +1,9 @@
-`use strict`
+`use strict`;
 // 1 Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
 
 let introSentence = `hello, my name is Fran and I am`;
 let age = 25;
-let fullSentence = `${introSentence} ${age}`; // operator (+) adds string & string
+let fullSentence = introSentence + " " + age; // operator (+) adds string & string
 
 console.log(fullSentence);
 
@@ -22,16 +22,21 @@ console.log(varResult);
 
 // 3 Remain there
 // checking if numbers are even
-let a = 30
-let b = 939
-let c = 40.9
+let a = 30;
+let b = 939;
+let c = 40.9;
 
 
-let resultA = a % 2
-let resultB = b % 2
-let resultC = c % 2
+let resultA = a % 2;
+let resultB = b % 2;
+let resultC = c % 2;
 console.log(`
-    ${a} ${(resultA = 0 ? "is Even" : "is Not even")}
-    ${b} ${(resultB = 0 ? "is Even" : "is Not even")}
-    ${c} ${(resultB = 0 ? "is Even" : "is Not even")}
+    ${a} ${(resultA ? "is Not even" : "is Even")}
+    ${b} ${(!resultB ? "is Even" : "is Not even")}
+    ${c} ${(resultC === 0 ? "is Even" : "is Not even")}
 `);
+
+// if i want equal 0 pls use === >>> ${a} ${(resultA ? === 0 "is Even" : "is Not even")}
+
+// line 35 solution Nr2 >> zero === is not even
+// line 36 solution Nr3 
