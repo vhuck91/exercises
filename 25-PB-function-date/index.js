ad48b4e7e4a47dc24cec5d2621c3c6e2bcf3efb6
Vadim Huck <back.end@hotmail.com>
2020-06-08 13:02:27 +0200

    task 25 with class

--- 25-PB-function-date/index.js
+++ 25-PB-function-date/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+const currentDate = new Date(); // set date
+const bookingDate = new Date(2019, 2, 12);
+let nameUser = "Marija";
+
+const bookEventDate = function (name, date, bookedDate) {
+    if (date > bookedDate) {
+        console.log(`${name}, date is already taken`);
+    } else {
+        console.log(`${name}, we signed you up for the ${bookedDate}`);
+    }
+
+}
+bookEventDate(nameUser, currentDate, bookingDate);
+bookEventDate("Vadim", new Date(), new Date(2020, 3, 27));
+
+/* my first try, should note use vars inside cuz they become scoped 
 const bookEventDate = function (name, date) {
     const currentDate = new Date(); // set date
     const bookingDate = "2021-06-08";
@@ -11,3 +27,4 @@ const bookEventDate = function (name, date) {
 
 } 
 bookEventDate("Marija", 2020);
+*/
